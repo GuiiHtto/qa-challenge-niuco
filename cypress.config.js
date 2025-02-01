8fdbf02711fba5e281c0dbb8782ca91d9cf6adb7
GuiiHtto <guilherme.competi@gmail.com>
2025-02-01 15:11:49 -0300

    Add environment variable for Mesa loader and improve test result verification

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress');
+const { exec } = require('child_process');
 
 module.exports = defineConfig({
   projectId: 'skqidb',
@@ -9,6 +10,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       on('after:run', (results) => {
+        exec('echo "Test run completed"');
+        
         if (results.totalFailed === 0) {
           exec('git checkout main && git pull && npm run test', (err, stdout, stderr) => {
             if (err) {
