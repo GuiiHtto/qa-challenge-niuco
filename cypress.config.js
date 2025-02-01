185d7579cdce517df18c94879da4477d2f4666be
GuiiHtto <guilherme.competi@gmail.com>
2025-02-01 12:45:37 -0300

    modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
 const { defineConfig } = require("cypress");
+const { exec } = require('child_process');
 
 module.exports = defineConfig({
   projectId: 'skqidb',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('after:run', (results) => {
+        if (results.totalFailed === 0) {
+          exec('git checkout main && git pull && npm run test', (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${err}`);
+              return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+          });
+        }
+      });
     },
   },
 });
