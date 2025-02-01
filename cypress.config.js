409064d1f7e4b236aabcddc76facc7ef4f465af8
GuiiHtto <guilherme.competi@gmail.com>
2025-02-01 15:35:38 -0300

    Update Cypress configuration to include timestamped folders for screenshots and videos localy

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,23 @@
 const { defineConfig } = require('cypress');
 const { exec } = require('child_process');
+const path = require('path');
+
+// Função para gerar um timestamp
+function getTimestamp() {
+  const now = new Date();
+  return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}_${String(now.getHours()).padStart(2, '0')}-${String(now.getMinutes()).padStart(2, '0')}-${String(now.getSeconds()).padStart(2, '0')}`;
+}
+
+// Diretórios com timestamp
+const timestamp = getTimestamp();
+const screenshotsFolder = `cypress/screenshots/${timestamp}`;
+const videosFolder = `cypress/videos/${timestamp}`;
 
 module.exports = defineConfig({
   projectId: 'skqidb',
   video: true,
-  screenshotsFolder: 'cypress/screenshots',
-  videosFolder: 'cypress/videos',
+  screenshotsFolder,
+  videosFolder,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
